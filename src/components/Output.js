99b4066b387b2b802aee221edae8a9165441529c
Dianka-ea <dmsoumare@sooja.fr>
2021-10-19 19:03:28 +0200

    working on tabs

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { filters } from '../filters';
-import { Tab, Loader, Dimmer, Card, Icon, Container, Menu, Label } from 'semantic-ui-react';
+import { Tab, Loader, Dimmer, Card, Icon, Container, Menu, Label, Segment } from 'semantic-ui-react';
 import CollectorTable from './CollectorTable';
 
 export default function Output(props) {
@@ -18,6 +18,20 @@ export default function Output(props) {
     //     }
     //     )]
 
+    // const panes = (data) => {
+    //     return [
+    //         {
+    //             menuItem: { key: data.label },
+    //             render: () => <Tab.Pane attached={false}><CollectorTable /></Tab.Pane>
+    //         }
+    //     ]
+    // }
+
+    const panes = [
+        { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+        { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+        { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    ]
     return (
         <div className="output-section">
             <Dimmer.Dimmable as={Container} dimmed={loading}>
@@ -36,44 +50,32 @@ export default function Output(props) {
                                 </Card>
                             )}
                         </Card.Group>
-                        {/* <Tab menu={ attached= true, tabular=true, content } />
-                            {filters.map((filter, index) => {
-                                <Menu.Item pointing secondary key='messages'>
-                                    {filter.label}<Label>count</Label>
-                                </Menu.Item>
-                                // <Tab.Pane>
-                                //     toto
-                                // </Tab.Pane>
-                            })}
-                        </Tab> */}
-                        {/* <Accordion
-                            fluid
-                            // styled
-                            exclusive={false}
-                            style={{ margin: "2rem 0" }}
-                        >
-                            {filters.map((filter, index) =>
-                                <div>
-                                    {Object.keys(props.data).includes(filter.name) ?
-                                        <Card fluid>
-                                            <Accordion.Title key={index}>
-                                                <Icon size='large' name="dropdown"/>
-                                                {filter.label}
-                                            </Accordion.Title>
-                                            <Accordion.Content>
-                                                <p>toto</p>
-                                            </Accordion.Content>
-                                        </Card>
-                                    : null}
-                                </div>
-                            )}
-                        </Accordion> */}
-
-
                     </div>
                     : null
                 }
             </Dimmer.Dimmable>
+            <div>
+                <Menu attached='top' tabular>
+                    {filters.map((filter, index) =>
+                        <Menu.Item key={index} name={filter.name}>
+                            {filter.label}
+                        </Menu.Item>
+                    )}
+                </Menu>
+                <Segment attached='bottom'>
+                    There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which don't look even slightly believable.
+                    If you are going to use a passage of Lorem Ipsum, you need to be sure
+                    there isn't anything embarrassing hidden in the middle of text. All
+                    the Lorem Ipsum generators on the Internet tend to repeat predefined
+                    chunks as necessary, making this the first true generator on the
+                    Internet. It uses a dictionary of over 200 Latin words, combined with
+                    a handful of model sentence structures, to generate Lorem Ipsum which
+                    looks reasonable. The generated Lorem Ipsum is therefore always free
+                    from repetition, injected humour, or non-characteristic words etc.
+                </Segment>
+            </div>
         </div >
     )
 }
