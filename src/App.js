2342445335356d9e93cb850c5a542f9a789069bc
Dianka-ea <dmsoumare@sooja.fr>
2021-10-20 18:28:55 +0200

    Centralize all components in Apps.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import Upload from './components/Upload';
+import SiteOutput from './components/site-centric/SiteOutput';
+import { Tab } from 'semantic-ui-react';
+import { useCallback, useState } from 'react';
+
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState({});
+  const callback = useCallback((data) => {
+    setData(data);
+  }, []);
+
+  const panes = [
+    { menuItem: 'Site-centric', render: () => <Tab.Pane className={"basic"} attached={false}><SiteOutput data={data} loading={loading} /></Tab.Pane> },
+    { menuItem: 'Ad-centric', render: () => <Tab.Pane className={"basic"} attached={false}>Tab 2 Content</Tab.Pane> },
+  ]
+
   return (
     <div className="App">
-      <Upload />
+      <Upload callback={callback} />
+      <Tab
+        menu={{ fluid: true, attached: false, vertical: true, tabular: true }}
+        panes={ panes }
+        grid={{ paneWidth: 14, tabWidth: 2 }}
+      />
     </div>
   );
 }
